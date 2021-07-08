933daff0884daa9de49c53d4b566e4c485abb2c0
Doug <dougparker.m@gmail.com>
2021-07-07 21:30:06 -0230

    Fumbling a little with js for some reason.

--- scripting.js
+++ scripting.js
@@ -1,2 +1,15 @@
 "use strict";
 
+const $ = (selector) => {document.querySelector(selector)};
+
+
+const testListener = () =>{
+	document.write(`<p>Hey there, cowboy</p>`);
+	// console.log("Hey there, cowboy.");
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+	testListener;
+	// $("#quizdrop").addEventListener("click", testListener);
+});
