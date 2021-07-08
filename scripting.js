f8bd351ee3da5ae1ec55d708ace8d0fff2b6a414
doug <dougparker.m@gmail.com>
2021-07-08 17:54:36 -0230

    JS loading values works. Polish needed but it should work.

--- scripting.js
+++ scripting.js
@@ -2,12 +2,28 @@
 
 const $ = (selector) => document.querySelector(selector);
 
+const geography = [
+	["Which of these African countries has the largest population?", "Egypt", "South Africa", "Ethiopia", "Nigeria", "3"],
+	["Which of these countries is NOT a member of the Caucases?", "Armenia", "Georgia", "Bulgaria", "Azerbaijan", "2"]
+]
+
 const toggleDropdown = () =>{
 	document.getElementById("droplinks").classList.toggle("show");
+}
+
+const setUpQuiz = (subject) =>{
+	let questions = [];
+
+	if(subject == "geo"){
+		questions = geography;
+	}
+
+	$("#question").innerHTML = questions[0][0];
+
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
 	$("#quizdrop").addEventListener("click", toggleDropdown);
-	$()
+	$("#geo").addEventListener("click", setUpQuiz("geo"));
 });
