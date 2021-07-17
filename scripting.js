814cb5d1ee951bd96d88f94124619cad12587698
doug <dougparker.m@gmail.com>
2021-07-16 22:48:11 -0230

    Quiz populates questions and evaluates if correct. Need to change alerts to something more elegant.

--- scripting.js
+++ scripting.js
@@ -3,27 +3,66 @@
 const $ = (selector) => document.querySelector(selector);
 
 const geography = [
-	["Which of these African countries has the largest population?", "Egypt", "South Africa", "Ethiopia", "Nigeria", "3"],
-	["Which of these countries is NOT a member of the Caucases?", "Armenia", "Georgia", "Bulgaria", "Azerbaijan", "2"]
-]
+	["Which of these African countries has the largest population?", "Egypt", "S. Africa", "Ethiopia", "Nigeria", "Nigeria"],
+	["Which of these countries is NOT a member of the Caucases?", "Armenia", "Georgia", "Bulgaria", "Azerbaijan", "Bulgaria"]
+];
+
+const literature = [];
+
+const history = [];
 
 const toggleDropdown = () =>{
 	document.getElementById("droplinks").classList.toggle("show");
 }
 
-const setUpQuiz = (subject) =>{
+const chooseRandomQuestion = (questions) =>{
+	var random = Math.floor(Math.random() * questions.length);
+	return questions[random];
+}
+
+const checkAnswer = (evt, question) =>{
+	var userAnswer = evt.target.textContent;
+	if(question[5] == userAnswer){
+		alert("Correct!");
+	}else{
+		alert("Wrong!");
+	}
+}
+
+const setUpQuiz = (evt) =>{
 	let questions = [];
 
-	if(subject == "geo"){
+	//CATCH ID OF CLICKED OPTION
+	if(evt.target.id == "geo"){
 		questions = geography;
+	}else if(evt.target.id == "lit"){
+		questions = literature;
+	}else if(evt.target.id == "his"){
+		questions = history;
 	}
 
-	$("#question").innerHTML = questions[0][0];
+	//GENERATE RANDOM NUMBER
+	var question = chooseRandomQuestion(questions)
 
+	//WRITE VALUES TO ELEMENTS
+	$("#question").textContent = question[0];
+	$("#Q1").textContent = question[1];
+	$("#Q2").textContent = question[2];
+	$("#Q3").textContent = question[3];
+	$("#Q4").textContent = question[4];
+
+	const elements = document.querySelectorAll(".questionBox");
+	elements.forEach(element => {
+		element.addEventListener("click", (e) =>{
+			checkAnswer(e, question);
+		});
+	})
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
 	$("#quizdrop").addEventListener("click", toggleDropdown);
-	$("#geo").addEventListener("click", setUpQuiz("geo"));
+	$("#geo").addEventListener("click", setUpQuiz);
+	$("#lit").addEventListener("click", setUpQuiz);
+	$("#his").addEventListener("click", setUpQuiz);
 });
